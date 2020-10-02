4965f1bc48bec381f5bcb8a1e1e411fce5fd0f84
vikashsurin <vikashsurin10@gmail.com>
2020-10-02 17:44:58 +0530

    updated files

--- node/index.js
+++ node/index.js
@@ -2,9 +2,33 @@ const express = require("express");
 const app = express();
 
 app.get("/",(req,res)=>{
-    res.send("<h1>Visiting Route</h1>");
-
+res.json({
+    message:"You are visiting route."
+})
 })
 
+app.get("/",(req,res)=>{
+res.json({
+    message:"You are visiting route."
+})
+})
+
+app.get("/login",(req,res)=>{
+res.json({
+    message:"You are visiting login."
+})
+})
+
+app.get("/signup",(req,res)=>{
+res.json({
+    message:"You are visiting signup."
+})
+})
+
+app.get("/logout",(req,res)=>{
+res.json({
+    message:"You are visiting logout."
+})
+})
 
 app.listen(8000, ()=>console.log("started node server at port :8000"));
